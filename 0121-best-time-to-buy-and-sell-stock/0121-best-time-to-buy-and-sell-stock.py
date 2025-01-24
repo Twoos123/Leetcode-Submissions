358699bc597e85cf2c69f286238c8d8e6e87de78
Asad Ali <68927600+Twoos123@users.noreply.github.com>
2025-01-24 02:53:25 -0500

    Time: 91 ms (55.51%), Space: 26.9 MB (51.8%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        maxP = 0
-        l, r = 0, 1 #left is buying, right is selling
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxP = max(maxP, profit)
-            else:
-                l = r
-            r += 1
-        return maxP
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1 #left = buy, right = sell
+        maxProfit = 0
 
-        
+        while r < len(prices): #while price to sell is less than the length of prices
+            if prices[l] < prices[r]: #while there is potential to make profit, (buy and then sell)
+                profit = prices[r] - prices[l] #calculate profit
+                maxProfit = max(maxProfit, profit) #take max of profit and maxprofit
+            else:
+                l = r #if no profit can be made, make left and right go to the same place to go to minimum
+            r += 1 #update right pointer till the end of the list
+        return maxProfit #return maxprofit
+
+       
