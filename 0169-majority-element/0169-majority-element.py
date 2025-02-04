36eb53d18f6fae96dc2b8101662ce0b02d34173a
Asad Ali <68927600+Twoos123@users.noreply.github.com>
2025-02-04 18:47:57 -0500

    Time: 6 ms (61.64%), Space: 19.3 MB (74.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,13 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums.sort()
-        return nums[len(nums) // 2]
+from collections import defaultdict
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        candidate, count = None, 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
