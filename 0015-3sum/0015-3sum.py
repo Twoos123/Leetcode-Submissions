02dec15ab4e6c042cce9cdfb33250e231ba89b47
Asad Ali <68927600+Twoos123@users.noreply.github.com>
2026-09-20 00:30:41 -0400

    Time: 1076 ms (14.75%), Space: 22.3 MB (33.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,36 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        res = []
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+
         nums.sort()
+        res = []
 
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
+            l = i + 1
+            r = len(nums) - 1
             
-            l, r = i + 1, len(nums) - 1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    res.append([a, nums[l], nums[r]])
+
+                if nums[i] + nums[l] + nums[r] == 0:
+                    res.append([nums[i], nums[l], nums[r]])
                     l += 1
 
-                    while nums[l] == nums [l - 1] and l < r:
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
+                        
+                elif nums[i] + nums[l] + nums[r] < 0:
+                    l += 1
+                elif nums[i] + nums[l] + nums[r] > 0:
+                    r -= 1
+
         return res
+
+
+            
+            
+
+
+      
         
-
-
